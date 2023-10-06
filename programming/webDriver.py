07a2258baf9ff4b6ecff308d6b75bd1903f0eb9f
just_monika <1664382962@qq.com>
2023-10-06 23:16:19 +0800

    更新：添加了打分题的处理方法，并优化了代码结构

--- programming/webDriver.py
+++ programming/webDriver.py
@@ -58,44 +58,74 @@ class webRun:
                             else:
                                 self.path = path
                                 break
-                    elementFlag = True
-                    try:
+                    elementFlag = 1
+                    try:  # 先找该题的题型，1=填空，3=选择，5=打分(是字符串类型的值)
+                        ele = driver.find_element(By.XPATH,
+                                                  f"/html/body/div[1]/form/div[13]/div[{self.path}]/fieldset/div[{i}]")
+                        print(f"找到第{i}题对应的元素")
+                    except:
+                        print(f"未找到第{i}题对应的元素，将跳过")
+                        self.selections.append(0)
+                        self.passtime += 1
+                        continue
+                    else:
+                        try:
+                            elementFlag = ele.get_attribute("type")
+                        except:
+                            continue
+                        if elementFlag is None:
+                            print(f"第{i}题元素异常，将跳过")
+                            self.selections.append(0)
+                            continue
+                        print(elementFlag)
+
+                    className = ""
+                    if elementFlag == '3':  # 选择题题型处理
                         element = driver.find_element(By.XPATH,
                                                       f"/html/body/div[1]/form/div[13]/div[{self.path}]/fieldset/div[{i}]/div[2]/div[1]/span/a")
-                    except:
-                        try:
-                            element = driver.find_element(By.XPATH,
-                                                          f"/html/body/div[1]/form/div[13]/div[{self.path}]/fieldset/div[{i}]/div[2]/input")
-                        except:
-                            print(f"未找到第{i}题对应的元素，将跳过")
-                            self.passtime += 1
-                            continue
-                        else:
-                            elementFlag = False
-                    className = str(element.get_attribute("class"))
-                    print(className)
+                        className = str(element.get_attribute("class"))  # 如果是选择题，则根据class的值判断是单选还是多选
+                        print(className)
+                        if time == 1:  # 如果是选择题，则查询选项个数(仅第一遍进入，后续直接读取存入的信息）
+                            selections = 0
+                            for j in range(1, 10):
+                                try:
+                                    text = WebDriverWait(driver, self.timeout).until(
+                                        EC.presence_of_element_located((By.XPATH,
+                                                                        f"/html/body/div[1]/form/div[13]/div[{self.path}]/fieldset/div[{i}]/div[2]/div[{j}]/div[1]"))
+                                    ).text
+                                    print(text)
+                                except TimeoutException:
+                                    print(f"未在 {self.timeout} 秒内找到元素")
+                                    break
+                                else:
+                                    if text.find("其他") == -1:
+                                        selections += 1
+                            self.selections.append(selections)
+                            print(f"第{i}题：题型‘{className}’,选项个数：{selections}")
+                        else:  # 如果是选择题，且不是第一遍填问卷，则直接读取第一遍存入的信息
+                            selections = self.selections[i - 1]
+                            print(f"第{i}题：题型‘{className}’,选项个数：{selections}")
 
-                    if time == 1 and elementFlag:
-                        selections = 0
-                        for j in range(1, 10):
+                        if className == "jqradio":  # 单选
+                            select = randint(1, selections)
+                            print(select)
                             try:
-                                text = WebDriverWait(driver, self.timeout).until(
-                                    EC.presence_of_element_located((By.XPATH,
-                                                                    f"/html/body/div[1]/form/div[13]/div[{self.path}]/fieldset/div[{i}]/div[2]/div[{j}]/div[1]"))
-                                ).text
-                                print(text)
-                            except TimeoutException:
-                                print(f"未在 {self.timeout} 秒内找到元素")
-                                break
-                            else:
-                                if text.find("其他") == -1:
-                                    selections += 1
-                        self.selections.append(selections)
-                        print(f"第{i}题：题型‘{className}’,选项个数：{selections}")
-                    elif time != 1 and elementFlag:
-                        selections = self.selections[i - 1]
-                        print(f"第{i}题：题型‘{className}’,选项个数：{selections}")
-                    else:
+                                driver.find_element(By.XPATH,
+                                                    f"/html/body/div[1]/form/div[13]/div[{self.path}]/fieldset/div[{i}]/div[2]/div[{select}]/span/a").click()
+                            except:
+                                continue
+                        elif className == "jqcheck":  # 多选
+                            selectTimes = randint(1, selections)
+                            integer_list = list(range(1, selections + 1))
+                            samples = sample(integer_list, selectTimes)
+                            print(samples)
+                            for select in samples:
+                                try:
+                                    driver.find_element(By.XPATH,
+                                                        f"/html/body/div[1]/form/div[13]/div[{self.path}]/fieldset/div[{i}]/div[2]/div[{select}]/span/a").click()
+                                except:
+                                    continue
+                    elif elementFlag == '1':  # 填空题题型处理
                         if time == 1:
                             self.selections.append(0)
                         try:
@@ -107,7 +137,7 @@ class webRun:
                         if self.AiFlag:
                             question = driver.find_element(By.XPATH,
                                                            f"/html/body/div[1]/form/div[13]/div[{self.path}]/fieldset/div[{i}]/div[1]").text
-                            print(f"找到填空题,题目：{question} 将填入AI的回答：")
+                            print(f"第{i}题，题型：“填空题”,题目：{question} 将填入AI的回答：")
                             tool = tools.questionnaireTools(self.apiKey)
                             for _ in range(1, 3):
                                 reply = "不知道"
@@ -118,36 +148,48 @@ class webRun:
                                 else:
                                     break
                         else:
+                            print(f"第{i}题，题型：“填空题”,未启用Ai回复功能，统一填入”不知道“")
                             reply = "不知道"
                         # input("")
                         driver.find_element(By.XPATH,
                                             f"/html/body/div[1]/form/div[13]/div[{self.path}]/fieldset/div[{i}]/div[2]/input").send_keys(
                             reply)
                         continue
-                    if className == "jqradio":  # 单选
+                    elif elementFlag == '5':  # 打分题题型处理
+                        if time == 1:  # 第一遍时先检查该题有几个选项并储存
+                            selections = 0
+                            for j in range(1, 10):
+                                try:
+                                    text = WebDriverWait(driver, self.timeout).until(
+                                        EC.presence_of_element_located((By.XPATH,
+                                                                        f"/html/body/div[1]/form/div[13]/div[{self.path}]/fieldset/div[{i}]/div[2]/div/ul/li[{j}]/a"))
+                                    ).text
+                                    print(text)
+                                except TimeoutException:
+                                    print(f"未在 {self.timeout} 秒内找到元素")
+                                    break
+                                else:
+                                    if text.find("其他") == -1:
+                                        selections += 1
+                            self.selections.append(selections)
+                            print(f"第{i}题：题型‘打分题’,选项个数：{selections}")
+                        else:
+                            selections = self.selections[i - 1]
+                            print(f"第{i}题：题型‘打分题’,选项个数：{selections}")
+
                         select = randint(1, selections)
                         print(select)
                         try:
                             driver.find_element(By.XPATH,
-                                                f"/html/body/div[1]/form/div[13]/div[{self.path}]/fieldset/div[{i}]/div[2]/div[{select}]/span/a").click()
+                                                f"/html/body/div[1]/form/div[13]/div[{self.path}]/fieldset/div[{i}]/div[2]/div/ul/li[{select}]/a").click()
                         except:
                             continue
-                    elif className == "jqcheck":  # 多选
-                        selectTimes = randint(1, selections)
-                        integer_list = list(range(1, selections + 1))
-                        samples = sample(integer_list, selectTimes)
-                        print(samples)
-                        for select in samples:
-                            try:
-                                driver.find_element(By.XPATH,
-                                                    f"/html/body/div[1]/form/div[13]/div[{self.path}]/fieldset/div[{i}]/div[2]/div[{select}]/span/a").click()
-                            except:
-                                continue
+
                     # input("按任意键继续")
                 except:
                     break
 
-            # input("")
+            #input("")
             if time == 1:
                 for k in range(5, 15):
                     try:
